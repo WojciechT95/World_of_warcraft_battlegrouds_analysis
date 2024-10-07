8ee23436c8f34485eda0afdf3361295b724d3991
Wojciech Trybus <wojciecht1995@gmail.com>
2024-10-07 20:17:22 +0200

    Update World_of_warcraft_battleground_analysis.sql

--- World_of_warcraft_battleground_analysis.sql
+++ World_of_warcraft_battleground_analysis.sql
@@ -18,7 +18,7 @@ group by 1
 
 /* 2. According to question one, how many times alliance or horde wins but as a full-fledged victory?
  
- (note, if there are not enough players in battleground, the game ends after 3 minutes, leaving the winner with the best progress so far.
+ (note, if there are not enough players in battleground, the game ends after 3 minutes, leaving the winner with the best progress so far or when the allotted time expires.
  Full-fledged win in Warsong Gulch occurs when a team captures the flag from the opponent's base to its base three times)  */ 
 
 select 
